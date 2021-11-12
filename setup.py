26e349017b738e627e832bcf0cc5e07db22d0db1
Mr-Milk <yb97643@um.edu.mo>
2021-11-12 11:40:04 +0800

    new distribute text

--- setup.py
+++ setup.py
@@ -1,26 +1,3 @@
-import os
-from pathlib import Path
+import setuptools
 
-from setuptools import find_packages, setup
-
-README = Path("README.md").read_text()
-
-setup(name="milkviz",
-      packages=find_packages(),
-      description="self-opinionated miscellaneous visualizations library in python",
-      long_description=README,
-      long_description_content_type="text/markdown",
-      version="0.3.0",
-      author="Mr-Milk",
-      url="https://github.com/Mr-Milk/milkviz",
-      author_email="yb97643@um.edu.mo",
-      license="MIT",
-      classifiers=[
-          "License :: OSI Approved :: Apache Software License",
-          "Programming Language :: Python :: 3",
-          "Intended Audience :: Science/Research",
-          "Topic :: Scientific/Engineering :: Bio-Informatics",
-      ],
-      python_requires='>=3.7',
-      install_requires=['numpy', 'pandas', 'matplotlib', 'seaborn', 'networkx', 'natsort', 'matplotlib-venn', 'upsetplot'],
-      )
+setuptools.setup()
