2e6d33aa5b78f720387fe04fe6779b025c1c0e5a
Mr-Milk <zym.zym1220@gmail.com>
2022-10-12 18:28:21 +0800

    set version

--- docs/conf.py
+++ docs/conf.py
@@ -1,21 +1,5 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-from sphinx_gallery.sorting import ExplicitOrder
-from sphinx_gallery.scrapers import matplotlib_scraper
 import warnings
+from sphinx_gallery.scrapers import matplotlib_scraper
 
 warnings.filterwarnings("ignore", category=UserWarning)
 
@@ -32,11 +16,11 @@ class matplotlib_svg_scraper(object):
 # -- Project information -----------------------------------------------------
 
 project = 'milkviz'
-copyright = '2021, Mr-Milk'
+copyright = '2022, Mr-Milk'
 author = 'Mr-Milk'
 
 # The full version, including alpha/beta/rc tags
-release = '0.5.1'
+release = '0.6.0'
 
 # -- General configuration ---------------------------------------------------
 
